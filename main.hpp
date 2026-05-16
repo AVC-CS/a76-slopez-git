551a382c1f3908b621cf1c408a8d5c955b0b1afd
Saml <slopez152@students.avc.edu>
2026-05-15 23:48:25 -0700

    A76

--- main.hpp
+++ main.hpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 const int N = 5000;
@@ -19,12 +20,37 @@ void printoutcontents(string, string, int, string, int);
 
 int makeNameRecord(string state[], string gender[], int year[], string name[], int count[])
 {
-    // TODO: open "babyname.txt", read each line into the parallel arrays,
+    ifstream inputfile("babyname.txt");
+    if (!inputfile)
+    {
+        cerr << "Error opening babyname.txt" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int cnt = 0;
+    while (cnt < N && inputfile >> state[cnt] >> gender[cnt] >> year[cnt] >> name[cnt] >> count[cnt])
+    {
+        cnt++;
+    }
+
+    inputfile.close();
+    return cnt;
 }
 
 int findNames(int cnt, string state[], string gender[], int year[], string name[], int count[], char starting, string stname)
 {
-    // TODO: print each record where the state matches `stname` and the name
+    int matches = 0;
+    cout << "The list of names in " << stname << " and starting with " << starting << endl;
+    for (int i = 0; i < cnt; i++)
+    {
+        if (state[i] == stname && name[i].rfind(string(1, starting), 0) == 0)
+        {
+            printoutcontents(state[i], gender[i], year[i], name[i], count[i]);
+            matches++;
+        }
+    }
+    cout << "total lines: " << matches << endl;
+    return matches;
 }
 
 void printoutallrecords(int cnt, string state[], string gender[], int year[], string name[], int count[])
